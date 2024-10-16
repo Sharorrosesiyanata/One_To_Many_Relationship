73a5323fc1fec75f15f4bf4fbff6abc986696140
Sharonrosesiyanata <sharonrosesiyanata7@gmail.com>
2024-10-16 12:55:53 +0200

    commit

---
+++ src/models/Comment.js
@@ -0,0 +1,13 @@
+//Normilization
+const mongoose = require("mongoose");
+
+const Comment = mongoose.model(
+  "Comment",
+  new mongoose.Schema({
+    username: String,
+    text: String,
+    createdAt: Date
+  })
+);
+
+module.exports = Comment;
---
+++ src/server.js
@@ -0,0 +1,111 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/bezkoder_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Successfully connect to MongoDB."))
+  .catch(err => console.error("Connection error", err));
+
+// const mongoose = require("mongoose");
+
+const db = require("./models");
+
+const createTutorial = function (tutorial) {
+  return db.Tutorial.create(tutorial).then(docTutorial => {
+    console.log("\n>> Created Tutorial:\n", docTutorial);
+    return docTutorial;
+  });
+};
+
+const createImage = function (tutorialId, image) {
+  return db.Image.create(image).then(docImage => {
+    console.log("\n>> Created Image:\n", docImage);
+    return db.Tutorial.findByIdAndUpdate(
+      tutorialId,
+      {
+        $push: {
+          images: {
+            _id: docImage._id,
+            url: docImage.url,
+            caption: docImage.caption
+          }
+        }
+      },
+      { new: true, useFindAndModify: false }
+    );
+  });
+};
+
+const createComment = function (tutorialId, comment) {
+  return db.Comment.create(comment).then(docComment => {
+    console.log("\n>> Created Comment:\n", docComment);
+
+    return db.Tutorial.findByIdAndUpdate(
+      tutorialId,
+      { $push: { comments: docComment._id } },
+      { new: true, useFindAndModify: false }
+    );
+  });
+};
+
+const createCategory = function(category) {
+  return db.Category.create(category).then(docCategory => {
+    console.log("\n>> Created Category:\n", docCategory);
+    return docCategory;
+  });
+};
+
+const addTutorialToCategory = function(tutorialId, categoryId) {
+  return db.Tutorial.findByIdAndUpdate(
+    tutorialId,
+    { category: categoryId },
+    { new: true, useFindAndModify: false }
+  );
+};
+
+// const getTutorialsInCategory = function(categoryId) {
+//   return db.Tutorial.find({ category: categoryId })
+//     .populate("category", "name -_id")
+//     .select("-comments -images -__v");
+// };
+
+//This is the time to use populate() function to get full Tutorial data.
+// const getTutorialWithPopulate = function (id) {
+//   return db.Tutorial.findById(id).populate("comments");
+// };
+
+//If you don’t want to get comment._id & comment.__v in the result, just add second parameters to populate() function.
+const getTutorialWithPopulate = function(id) {
+  return db.Tutorial.findById(id).populate("comments", "-_id -__v");
+};
+
+const run = async function() {
+  var tutorial = await createTutorial({
+    title: "Tutorial #1",
+    author: "bezkoder"
+  });
+
+ var category = await createCategory({
+    name: "Node.js",
+    description: "Node.js tutorial"
+  });
+
+  tutorial = await addTutorialToCategory(tutorial._id, category._id);
+  console.log("\n>> Tutorial:\n", tutorial);
+  //This is the time to use populate() function to get full Tutorial data.
+  tutorial = await getTutorialWithPopulate(tutorial._id);
+  console.log("\n>> populated Tutorial:\n", tutorial);
+};
+
+mongoose
+  .connect("mongodb://localhost/bezkoder_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Successfully connect to MongoDB."))
+  .catch(err => console.error("Connection error", err));
+
+run();
+
